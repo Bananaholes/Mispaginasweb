da2edb946647684f787c3660360fa600811282c6
Bananaholes <germanicocapo@gmail.com>
2025-04-06 22:10:28 -0300

    Solucione el problema del index.html

--- index.py
+++ index.py
@@ -16,9 +16,15 @@ def mostrarLenguajes():
 
 @app.route('/')
 def principio():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 
 if __name__=="__main__":
     app.run(debug=True, port=5017)
+
+
+import os
+
+print(os.getcwd())  # Te dice en qué carpeta estás ejecutando el script
+print(os.listdir('templates'))  # Lista los archivos dentro de /templates
